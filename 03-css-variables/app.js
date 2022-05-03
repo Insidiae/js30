12f9bb7fcaa062d266eee6d3a6a985c42648e5f7
Kobe Ruado <insidiae423@gmail.com>
2022-05-03 10:32:10 +0800

    03 - Playing with CSS Variables and JS

---
+++ 03-css-variables/app.js
@@ -0,0 +1,12 @@
+const allInputs = document.querySelectorAll(".controls input");
+
+function handleUpdate(event) {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    `${this.value}${suffix}`
+  );
+}
+
+allInputs.forEach((input) => input.addEventListener("change", handleUpdate));
+allInputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
